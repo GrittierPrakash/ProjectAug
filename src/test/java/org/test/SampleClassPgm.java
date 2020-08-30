eb2dc5dd01cac894ce059fa7855b237b8108adce
GrittierPrakash <grittierprakash@gmail.com>
2020-08-30 17:38:35 +0530

    code updated

--- src/test/java/org/test/SampleClassPgm.java
+++ src/test/java/org/test/SampleClassPgm.java
@@ -9,10 +9,9 @@ public class SampleClassPgm {
 		char c = s.charAt(2);
 		System.out.println(c);
 		
-
-
 	System.out.println("hi");
 	System.out.println("hello");
+	System.out.println("this is sample program to check conflict");
 
 }
 }
