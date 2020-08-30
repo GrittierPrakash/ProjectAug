f4ed65e88004060290227638a91101baf07c6029
GrittierPrakash <grittierprakash@gmail.com>
2020-08-30 15:08:55 +0530

    code is completed

--- src/test/java/org/test/SampleClassPgm.java
+++ src/test/java/org/test/SampleClassPgm.java
@@ -1,5 +1,12 @@
 package org.test;
 
 public class SampleClassPgm {
-
+public static void main(String[] args) {
+	String s = "grittier";
+	int l = s.length();
+	System.out.println(l);
+	char c = s.charAt(2);
+	System.out.println(c);
+	
+}
 }
