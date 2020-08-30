3fcc5306987b041ada32fc210773bf7a6d213d99
GrittierPrakash <grittierprakash@gmail.com>
2020-08-30 16:30:17 +0530

    new code

--- src/test/java/org/test/SampleClassPgm.java
+++ src/test/java/org/test/SampleClassPgm.java
@@ -2,11 +2,7 @@ package org.test;
 
 public class SampleClassPgm {
 public static void main(String[] args) {
-	String s = "grittier";
-	int l = s.length();
-	System.out.println(l);
-	char c = s.charAt(2);
-	System.out.println(c);
-	
+	System.out.println("hi");
+	System.out.println("hello");
 }
 }
